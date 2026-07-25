48642fb44e80951d01f46358c2c5d3cf7a159fb4
benjamin427 <shadowboxer79@hotmail.com>
2026-07-25 13:15:50 -0400

    Made corrections to this file

--- test/google.spec.js
+++ test/google.spec.js
@@ -3,7 +3,7 @@ import {test, test as it, expect} from '@playwright/test'
 
 test.describe("Using the Google search engine", () => {
     it("Go to the site" , async({page}) => {
-        await page.goto("https://www.gooogle.com")
+        await page.goto("https://www.google.com")
     })
     it("Enter search field with random texts", async({page}) => {
         await page.locator('#APjFqb').fill("salesforce")
