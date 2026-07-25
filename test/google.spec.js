38c4da89417654afa9ae833d6dd363f040915c8d
benjamin427 <shadowboxer79@hotmail.com>
2026-07-25 12:33:59 -0400

    Made modifications for this file

--- test/google.spec.js
+++ test/google.spec.js
@@ -1,14 +1,14 @@
 import {test, expect} from '@playwright/test'
 
 
-test.describe("Using the Google search engine", async({page}) => {
-    it("Go to the site" , async() => {
+test.describe("Using the Google search engine", async() => {
+    it("Go to the site" , async({page}) => {
         await page.goto("https://www.gooogle.com")
     })
-    it("Enter search field with random texts", async() => {
+    it("Enter search field with random texts", async({page}) => {
         await page.locator('#APjFqb').fill("salesforce")
     })
-    it("Click the search button", async() => {
+    it("Click the search button", async({page}) => {
         await page.getByRole('button', {name: 'Google Search'})
     })
 })
